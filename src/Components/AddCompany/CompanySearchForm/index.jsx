f09ba0a593fdd68d13da056c735b0a1f4a3bc99b
Adam Pielach <adampielach@gmail.com>
2019-09-15 23:13:35 +0200

    trying to merge api responses into one

--- src/Components/AddCompany/CompanySearchForm/index.jsx
+++ src/Components/AddCompany/CompanySearchForm/index.jsx
@@ -2,11 +2,40 @@ import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 
+import API from "../../../Helpers/Api";
+
 export default function() {
   const [companyInput, setCompanyInput] = useState("");
 
-  const addCompany = function(e) {
+  const addCompany = async function(e) {
     e.preventDefault();
+    if (companyInput !== "") {
+      let symbolResponse = await API.stocks.getCompanyData(
+        "SYMBOL_SEARCH",
+        "keywords=" + companyInput
+      );
+
+      let globalResponse = await API.stocks.getCompanyData(
+        "GLOBAL_QUOTE",
+        "symbol=" + companyInput
+      );
+
+      let mergedResponse = {
+        ...symbolResponse.bestMatches[0],
+        ...globalResponse["Global Quote"]
+      };
+
+      let globalResponseArray = {};
+
+      for (let key of Object.entries(mergedResponse)) {
+        let currentKey = key[0].substring(key[0].indexOf(" ") + 1);
+        globalResponseArray[currentKey] = key[1];
+      }
+      console.log(globalResponseArray);
+
+      return;
+    }
+    alert("Please provide appropriate symbol.");
   };
 
   return (
